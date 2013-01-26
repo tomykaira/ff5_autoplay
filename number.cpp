0a9cfbede2bbcefa977586a786a06adfc233c188
tomykaira <tomykaira@gmail.com>
2013-01-26 17:35:25 +0900

    判定結果の改善
    閾値を徐々に下げていくことで、似ている数字(8と9など)についても最適な解を求める

--- number.cpp
+++ number.cpp
@@ -20,32 +20,47 @@ main(int argc, char *argv[])
 
 	cv::Mat result_img;
 
-	for (int num = 0; num < 10; ++num) {
-		char template_file[1024];
-		sprintf(template_file, "number_data/%d.bmp", num);
+	for (double minScore = 1.0; minScore > 0.9; minScore -= 0.02) {
 
-		// テンプレート画像
-		cv::Mat tmp_img = cv::imread(template_file, 1);
-		if(!tmp_img.data) return -1;
+		for (int num = 0; num < 10; ++num) {
+			char template_file[1024];
+			char num_string[1024];
+			sprintf(template_file, "number_data/%d.bmp", num);
+			sprintf(num_string, "%d", num);
 
-		// 最大 50 個検出する
-		for ( int i=0; i<50; i++ ) {
-			// テンプレートマッチング
-			cv::matchTemplate(search_img, tmp_img, result_img, CV_TM_CCOEFF_NORMED);
-			// 最大のスコアの場所を探す
-			cv::Rect roi_rect(0, 0, tmp_img.cols, tmp_img.rows);
-			cv::Point max_pt;
-			double maxVal;
-			cv::minMaxLoc(result_img, NULL, &maxVal, NULL, &max_pt);
-			// 一定スコア以下の場合は処理終了
-			if ( maxVal < 0.9 ) break;
+			// テンプレート画像
+			cv::Mat tmp_img = cv::imread(template_file, 1);
+			if(!tmp_img.data) return -1;
 
-			roi_rect.x = max_pt.x;
-			roi_rect.y = max_pt.y;
-			std::cout << "(" << max_pt.x << ", " << max_pt.y << "), score=" << maxVal << std::endl;
-			// 探索結果の場所に矩形を描画
-			cv::rectangle(search_img0, roi_rect, cv::Scalar(0,255/10*num,255), 1);
-			cv::rectangle(search_img, roi_rect, cv::Scalar(0,0,255), CV_FILLED);
+			std::cout << "number " << num << std::endl;
+
+			// 最大 50 個検出する
+			for ( int i=0; i<50; i++ ) {
+				// テンプレートマッチング
+				cv::matchTemplate(search_img, tmp_img, result_img, CV_TM_CCOEFF_NORMED);
+				// 最大のスコアの場所を探す
+				cv::Rect roi_rect(0, 0, tmp_img.cols, tmp_img.rows);
+				cv::Point max_pt;
+				double maxVal;
+				cv::minMaxLoc(result_img, NULL, &maxVal, NULL, &max_pt);
+
+				// 一定スコア以下の場合は処理終了
+				if ( maxVal < minScore ) break;
+
+				roi_rect.x = max_pt.x;
+				roi_rect.y = max_pt.y;
+				std::cout << "\t(" << max_pt.x << ", " << max_pt.y << "), score=" << maxVal << std::endl;
+
+				if (maxVal < 0.999999)
+					cv::putText(search_img0, num_string, cvPoint(roi_rect.x, roi_rect.y), cv::FONT_HERSHEY_SIMPLEX, 0.6,
+					            CV_RGB(255, 0, 0));
+
+
+				// 探索結果の場所に矩形を描画
+				cv::rectangle(search_img0, roi_rect, cv::Scalar(0,255/10*num,255), 1);
+				cv::rectangle(search_img, roi_rect, cv::Scalar(0,0,255), CV_FILLED);
+			}
+
 		}
 
 	}
